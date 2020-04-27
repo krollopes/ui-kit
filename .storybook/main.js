eea500fdd785fad9dae478f32953046e640849a8
Caroline <caroline.lopes@nexojornal.com.br>
2020-04-27 18:31:44 -0300

    Code Challenge UI Kit react com storybook

---
+++ .storybook/main.js
@@ -0,0 +1,20 @@
+const path = require('path');
+
+module.exports = {
+  webpackFinal: async config => {
+    config.module.rules.push({
+      test: /\.(ts|tsx)$/,
+      include: [path.resolve(__dirname, '..')],
+      use: [
+        {
+          loader: require.resolve('ts-loader'),
+        },
+        {
+          loader: require.resolve('react-docgen-typescript-loader'),
+        },
+      ],
+    });
+    config.resolve.extensions.push('.ts', '.tsx');
+    return config;
+  },
+};
